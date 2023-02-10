efd1736a7aa8a2a1db6bd3f4af80f6d4b383af39
sivaaravindh <sivaaravind262003@gmail.com>
2023-02-10 09:58:36 +0530

    index

--- main.js
+++ main.js
@@ -1,17 +1,66 @@
-function startTime() {
-    var today = new Date();
-    var hur = today.getHours();
-    var min = today.getMinutes();
-    var sec = today.getSeconds();
-    min = checkTime(min);
-    sec = checkTime(sec);
-    document.getElementById('txt').innerHTML =
-    hur + ":" + min + ":" + sec;
-    var t = setTimeout(startTime, 500);
+// function startTime() {
+let day=document.getElementById('date');
+let times=document.getElementById('time');
+let days=new Date();
+day.innerText=`${days.getDate()}: ${days.getMonth()+1}:${days.getFullYear()}`
+setInterval(()=>
+{
+    let date=new Date();
+    times.innerText=`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    let amid=document.getElementById('am');
+    let pmid=document.getElementById('pm');
+    if(date.getHours()<12)
+    {
+        amid.style.backgroundColor="green";
     }
-    function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
-    };  
-    return i;
+    else
+    {
+        pmid.style.backgroundColor="green";
     }
+},);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var today = new Date();
+//     var hur = today.getHours();
+//     var min = today.getMinutes();
+//     var sec = today.getSeconds();
+//     min = checkTime(min);
+//     sec = checkTime(sec);
+//     document.getElementById('txt').innerHTML =
+//     hur + ":" + min + ":" + sec;
+//     var t = setTimeout(startTime, 500);
+//     }
+//     function checkTime(i) {
+//     if (i < 10) {
+//         i = "0" + i
+//     };  
+//     return i;
+//     }
